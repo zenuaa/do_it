e21cce07e6f8eb959b91e264baf6c4dc0c1642e1
zenua <zenuaa@outlook.com>
2023-03-10 10:27:44 +0200

    gitignore configuring

--- js/do.js
+++ js/do.js
@@ -1,35 +1,7 @@
 'use strict'
-let x = 10,
-    y = 1;
 
-console.log(`значение х = ${x} \n значение у = ${y} `);
+let poo = true,
+    foo = 12;
 
-const arr = [1, 'qwe', x, 12];
-
-console.log(arr[0] + arr[3]);
-
-
-const obj = {
-    name: 'zenua',
-    age: 34,
-    writeInfo: function () {
-        console.log(`Name is ${this.name} \n Age ${this.age}`);
-        console.log(this.age + 1);
-        this.daughtersCount = 3;
-    }
-}
-
-obj.writeInfo();
-console.log(obj);
-
-var obj = {
-    name: "zenua",
-    age: 34,
-    writeInfo: function () {
-        console.log("Name is " + this.name + " \n Age " + this.age);
-        console.log(this.age + 1);
-        this.daughtersCount = 3
-    }
-};
-obj.writeInfo();
-console.log(obj);
+    console.log(poo && foo);
+    
