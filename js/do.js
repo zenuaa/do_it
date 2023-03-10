c77c5c2fdbae2ff2318f2d0fbf16d3f834f7ddb6
zenua <zenuaa@outlook.com>
2023-03-10 11:23:03 +0200

    five commit

--- js/do.js
+++ js/do.js
@@ -4,32 +4,3 @@ let x = 10,
 
 console.log(`значение х = ${x} \n значение у = ${y} `);
 
-const arr = [1, 'qwe', x, 12];
-
-console.log(arr[0] + arr[3]);
-
-
-const obj = {
-    name: 'zenua',
-    age: 34,
-    writeInfo: function () {
-        console.log(`Name is ${this.name} \n Age ${this.age}`);
-        console.log(this.age + 1);
-        this.daughtersCount = 3;
-    }
-}
-
-obj.writeInfo();
-console.log(obj);
-
-var obj = {
-    name: "zenua",
-    age: 34,
-    writeInfo: function () {
-        console.log("Name is " + this.name + " \n Age " + this.age);
-        console.log(this.age + 1);
-        this.daughtersCount = 3
-    }
-};
-obj.writeInfo();
-console.log(obj);
