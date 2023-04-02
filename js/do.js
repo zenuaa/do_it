8452f1a59087a2c74379cbfbe9d60063a85695fa
zenua <zenuaa@outlook.com>
2023-04-03 00:05:32 +0300

    create db

--- js/do.js
+++ js/do.js
@@ -1,74 +1,36 @@
 'use strict'
+let personalMovieDB = {
+    count: undefined,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: !1,
+    countFilm: function () {
+        let numberOfFilms = +prompt("How mutch films do you see", "");
+        while (numberOfFilms == '' || numberOfFilms !== numberOfFilms || numberOfFilms == null) {
+            numberOfFilms = +prompt("How mutch films do you see", "");
+        }
+        personalMovieDB.count = numberOfFilms;
+        return this;
+    },
+    askAboutFilm: () => {
+        let a = prompt("The last movie what have you seen"),
+            b = +prompt("Rate the movie");
+        personalMovieDB.movies[a] = b;
+        return personalMovieDB
+    },
+    showDB: () => {
+        if (!personalMovieDB.privat) {
+            console.log(personalMovieDB);
+        }
+        return personalMovieDB;
+    },
+    askAboutGenres:function(){
+        this.genres.push(prompt('Your favorite genres'));
+    return this;
+    }
+};
 
-// var numberOfFilms = prompt("How mutch films do you see", ""),
-//   personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: !1,
-//   };
 
-// function ask() {
-//   var a = prompt("The last movie what have you seen"),
-//     b = prompt("Rate the movie");
-//   personalMovieDB.movies[a] = b;
-// }
-// ask();
-// ask();
+personalMovieDB.countFilm().askAboutFilm().askAboutGenres().showDB();
 
-// console.log(personalMovieDB);
-// var ask = confirm();
-
-// function askC() {
-//   console.log("Second chance");
-//   (ask = confirm()) == true ? console.log("Good boy") : console.log("YOU FAIL");
-// }
-// ask ? console.log("User accept") : ask || askC();
-
-// const foo = +'10';
-// switch (foo) {
-//   case 3:
-//     console.log('foo = 3');
-//     break;
-//   case 10:
-//     console.log('foo = 10');
-//     break;
-//   default:
-//     console.log('There is no match');
-//     break;
-// }
-
-// const foo = 10,
-//   poo = 100;
-
-// // if (foo < 10 || poo == 100) {
-// //   console.log('All right');
-// // } else {
-// //   console.log('no match');
-// // }
-
-// (foo < 10 || poo == 100) ? console.log('All right'): console.log('no match');
-
-for (let i = 1, x = 10; i < 10; i++) {
-  console.log(x);
-  x++;
-}
-
-let inf = {
-  x: 1,
-  c: 2,
-  v: 3,
-  b: 4,
-  obj: {
-    x: 10,
-    c: 20,
-    v: 30,
-    b: 40,
-  }
-}
-
-for (let i = 0, arr = Object.keys(inf); i < arr.length; i++) {
-  console.log(arr[i]);
-
-}
