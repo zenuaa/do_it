93c22739ac661fd2c622f43a7122d98ff13b90ed
zenua <zenuaa@outlook.com>
2023-04-15 23:43:01 +0300

    add properties for DB

--- js/do.js
+++ js/do.js
@@ -1,14 +1,18 @@
-'use strict'
+'use strict' 
 let personalMovieDB = {
-    count: undefined,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: !1,
+    privat: false,
+    start: function () {
+        this.countFilm().askAboutFilm().askAboutGenres().showDB();
+        return this;
+    },
     countFilm: function () {
-        let numberOfFilms = +prompt("How mutch films do you see", "");
-        while (numberOfFilms == '' || numberOfFilms !== numberOfFilms || numberOfFilms == null) {
-            numberOfFilms = +prompt("How mutch films do you see", "");
+        let numberOfFilms = prompt("How much films did you see", "");
+        while (numberOfFilms === null || numberOfFilms.trim() === '' || isNaN(+ numberOfFilms)) {
+            numberOfFilms = prompt("How much films did you see", "");
         }
         personalMovieDB.count = numberOfFilms;
         return this;
@@ -17,22 +21,28 @@ let personalMovieDB = {
         let b;
         for (let i = 0; 2 > i; i++) {
             b = prompt("The last film what you seen? " + i);
-            let c = +prompt("Rate this film " + i);
-            b && c && !(c !== c) ? 50 < b.length ? (console.log("The name length cannot be largest 50 simbols"), i--) : personalMovieDB.movies[b] = + c : (console.log("Aswer the question!!!"), i--);
+            let c = + prompt("Rate this film " + i);
+            b && c && !(c !== c) ? 50 < b.length ? (console.warn("The name length cannot be largest 50 simbols"), i --) : personalMovieDB.movies[b] = + c : (console.log("Aswer the question!!!"), i --);
         }
         return personalMovieDB
     },
     showDB: () => {
-        if (!personalMovieDB.privat) {
+        if (! personalMovieDB.privat) {
             console.log(personalMovieDB);
         }
         return personalMovieDB;
     },
     askAboutGenres: function () {
-        this.genres.push(prompt('Your favorite genres'));
+        let t = prompt('Please enter your favorite genre:');
+        for (t; t === null || t.trim() === '' || !isNaN(+ t) || t.length < 3;) {
+            console.warn('Please enter a valid input.');
+            t = prompt('Please enter your favorite genre:');
+        }
+        this.genres.push(t);
+        return this;
+    },
+    toggleVisibleDB: function () {
+        this.privat ? this.privat = false : this.privat = true;
         return this;
     }
 };
-
-personalMovieDB.countFilm().askAboutFilm().askAboutGenres().showDB();
-
